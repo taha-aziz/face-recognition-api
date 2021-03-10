55f13a66ee4432b56d7042f894341c8a2f66181a
Taha Aziz <25tahaaziz25@gmail.com>
2021-03-10 14:18:44 -0800

    data validation, move api key to backend, create endpoint for api

--- controllers/image.js
+++ controllers/image.js
@@ -1,10 +1,25 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+    apiKey: 'e0e73b018e614d3896a51e16a4366eed'
+});
+
+export const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to retrieve api data'))
+}
+
 export const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import knex from 'knex';
 import { handleRegister } from './controllers/register.js';
 import { handleSignin } from './controllers/signin.js';
 import { handleProfileGet } from './controllers/profile.js';
-import { handleImage } from './controllers/image.js';
+import { handleImage, handleApiCall } from './controllers/image.js';
 
 const db = knex({
     client: 'pg',
@@ -28,6 +28,7 @@ app.post('/signin', handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { handleApiCall(req, res) })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
