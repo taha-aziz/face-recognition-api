1d1175b1278851cf2e94fa6996bcfb022bb0c5cc
Taha Aziz <25tahaaziz25@gmail.com>
2021-03-11 17:21:45 -0800

    fix knex db connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
